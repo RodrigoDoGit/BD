aedf7e4dc7a0d85485d05d25d932cc390b53be38
Rodrigo Emídio <94897828+RodrigoDoGit@users.noreply.github.com>
2022-07-20 11:49:20 +0100

    Add files via upload

---
+++ Práticas/Folha 11/ex2.sql
@@ -0,0 +1,7 @@
+SELECT Title, StreamDate, Charge
+FROM MOVIE
+NATURAL JOIN STREAM
+NATURAL JOIN MOVIE_GENRE
+NATURAL JOIN GENRE
+WHERE Month(StreamDate) = 12 AND Year(StreamDate) = 2017 AND Label = 'Thriller'
+ORDER BY Title, StreamDate DESC;
